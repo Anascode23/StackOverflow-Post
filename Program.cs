5653d9155d28b2da0ce5ecf9cbd4beb780bf9089
anascode23 <anas.ali@ecutbildning.se>
2024-01-09 16:13:49 +0000

    project complete

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var stack = new StackOverFlowPost("Title", "Description", DateTime.Now);
+while (true)
+{
+    stack.Post();
+    Console.WriteLine("Please upvote or downvote this post");
+    var input = Console.ReadLine();
+
+    if (input == "exit")
+        break;
+
+    if (!string.IsNullOrEmpty(input))
+    {
+        stack.Upvote(input);
+        stack.Downvote(input);
+        stack.Totalvotes(input);
+    }
+}
+
---
+++ StackOverFlowPost.cs
@@ -0,0 +1,66 @@
+public class StackOverFlowPost
+{
+    private string _title;
+    private int _upvote;
+    private int _downvote;
+    private string _descrption;
+    private DateTime _created;
+    private string Title
+    {
+        get { return _title; }
+        set { _title = value; }
+    }
+    private string Descrption
+    {
+        get { return _descrption; }
+        set { _descrption = value; }
+    }
+    private DateTime Created
+    {
+        get { return _created; }
+        set { _created = value; }
+
+    }
+    private int Upvotes { get { return _upvote; } set { _upvote = value; } }
+    private int Downvotes { get { return _downvote; } set { _downvote = value; } }
+    public StackOverFlowPost(string title, string description, DateTime created)
+    {
+        Title = title;
+        Descrption = description;
+        Created = created;
+    }
+    public void Post()
+    {
+        Console.WriteLine("Created post");
+        Console.WriteLine("------------------------");
+        Console.WriteLine("Title: " + Title);
+        Console.WriteLine("Description: " + Descrption);
+        Console.WriteLine("Created: " + Created);
+        Console.WriteLine("------------------------");
+    }
+
+    public void Upvote(string userinput)
+    {
+        if (userinput == "u")
+            _upvote++;
+    }
+    public void Downvote(string userinput)
+    {
+        if (userinput == "d")
+            _downvote++;
+    }
+    public void Totalvotes(string userinput)
+    {
+        if (userinput == "votes")
+        {
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Total upvotes: " + Upvotes);
+            Console.WriteLine("Total downvotes:" + Downvotes);
+            Console.WriteLine("------------------------");
+            Console.WriteLine();
+        }
+
+
+
+    }
+}
